46f08c3dc15b9e1c1194e9645532e9c5bb0385d4
Juan González Guerrero <JuanGue@youforce.net>
2021-01-28 08:37:12 +0100

    Test repeated game

--- ScoreBoard/Service.cs
+++ ScoreBoard/Service.cs
@@ -1,15 +1,22 @@
-﻿
-using System;
+﻿using System;
+using System.Collections.Generic;
 
 namespace ScoreBoard
 {
     public class Service
     {
+        public SortedList<string, Game> Games = new SortedList<string, Game>();
+
         public void StartGame(Game game)
         {
             if (game == null || string.IsNullOrWhiteSpace(game.HomeTeam) || string.IsNullOrWhiteSpace(game.AwayTeam))
             {
                 throw new ArgumentNullException();
+            }
+
+            if (Games.ContainsKey($"{game.HomeTeam} - {game.AwayTeam}"))
+            {
+                throw new Exception("Game already started!");
             }
         }
     }
--- ScoreBoardTest/ServiceTests.cs
+++ ScoreBoardTest/ServiceTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ScoreBoard;
@@ -60,6 +61,33 @@ namespace ScoreBoardTest
 
             //Assert
             act.Should().Throw<ArgumentNullException>();
+        }
+
+        [TestMethod]
+        public void Service_StartGameWithExistingGame_ShouldThrowException()
+        {
+            //Arrange
+            var game1 = new Game
+            {
+                HomeTeam = "Mexico",
+                AwayTeam = "Canada"
+            };
+
+            var game2 = new Game
+            {
+                HomeTeam = "Mexico",
+                AwayTeam = "Canada"
+            };
+
+            //Act
+            _service.Games.Add($"{game1.HomeTeam} - {game1.AwayTeam}", game1);
+            Action act = () => _service.StartGame(game2);
+
+            //Assert
+            act.Should().Throw<Exception>().WithMessage("Game already started!");
+            _service.Games.Count.Should().Be(1);
+            _service.Games.Values.First().HomeTeam.Should().Be("Mexico");
+            _service.Games.Values.First().AwayTeam.Should().Be("Canada");
         }
     }
 }
