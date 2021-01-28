6e4be247a7008dfcbbe4ba519c4dcfbdf2b9f87b
Juan González Guerrero <JuanGue@youforce.net>
2021-01-28 10:27:48 +0100

    Refactor validations

--- ScoreBoard/Service.cs
+++ ScoreBoard/Service.cs
@@ -9,28 +9,28 @@ namespace ScoreBoard
 
         public void StartGame(Game game)
         {
-            ValidateGame(game);
+            ValidationExtensions.ValidateGame(game);
 
             var key = $"{game.HomeTeam} - {game.AwayTeam}";
 
-            if (GameExists(key)) throw new Exception("Game already started!");
+            if (ValidationExtensions.GameExists(key, Games)) throw new Exception("Game already started!");
 
             Games.Add(key, game);
         }
 
         public void FinishGame(string key)
         {
-            ValidateKey(key);
+            ValidationExtensions.ValidateKey(key);
 
-            if (!GameExists(key)) throw new Exception("Game does not exist!");
+            if (!ValidationExtensions.GameExists(key, Games)) throw new Exception("Game does not exist!");
 
             Games.Remove(key);
         }
 
         public void UpdateGame(string key, int homeTeamScore, int awayTeamScore)
         {
-            ValidateKey(key);
-            ValidateScores(homeTeamScore, awayTeamScore);
+            ValidationExtensions.ValidateKey(key);
+            ValidationExtensions.ValidateScores(homeTeamScore, awayTeamScore);
 
             var gameToUpdate = Games[key];
             if(gameToUpdate == null) throw new Exception();
@@ -38,35 +38,6 @@ namespace ScoreBoard
             gameToUpdate.HomeTeamScore = homeTeamScore;
             gameToUpdate.AwayTeamScore = awayTeamScore;
             gameToUpdate.TotalScore = homeTeamScore + awayTeamScore;
-        }
-
-        private void ValidateGame(Game game)
-        {
-            if (game == null || string.IsNullOrWhiteSpace(game.HomeTeam) || string.IsNullOrWhiteSpace(game.AwayTeam))
-            {
-                throw new ArgumentNullException();
-            }
-        }
-
-        private bool GameExists(string key)
-        {
-            return Games.ContainsKey(key);
-        }
-
-        private void ValidateKey(string key)
-        {
-            if (string.IsNullOrWhiteSpace(key))
-            {
-                throw new ArgumentNullException();
-            }
-        }
-
-        private void ValidateScores(int homeTeamScore, int awayTeamScore)
-        {
-            if (homeTeamScore < 0 || awayTeamScore < 0)
-            {
-                throw new ArgumentOutOfRangeException();
-            }
         }
     }
 }
---
+++ ScoreBoard/ValidationExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ScoreBoard
+{
+    public static class ValidationExtensions
+    {
+        public static void ValidateGame(Game game)
+        {
+            if (game == null || string.IsNullOrWhiteSpace(game.HomeTeam) || string.IsNullOrWhiteSpace(game.AwayTeam))
+            {
+                throw new ArgumentNullException();
+            }
+        }
+
+        public static bool GameExists(string key, IReadOnlyDictionary<string, Game> games)
+        {
+            return games.ContainsKey(key);
+        }
+
+        public static void ValidateKey(string key)
+        {
+            if (string.IsNullOrWhiteSpace(key))
+            {
+                throw new ArgumentNullException();
+            }
+        }
+
+        public static void ValidateScores(int homeTeamScore, int awayTeamScore)
+        {
+            if (homeTeamScore < 0 || awayTeamScore < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
